07faad82c4f6408ad6d0177e8a2a2ab89e6c3447
Anton <antosha.uvarov.90@mail.ru>
2024-10-24 01:04:06 +0300

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,4 @@
+const galleryContainer = document.querySelector('about__box');
+const galleryControlsContainer = document.querySelector('about__controls');
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelector('about__img');
